37741e1e197409634c7df7a2e2451d08560edbbd
Horst Gutmann <horst.gutmann@grafana.com>
2023-05-16 12:56:38 +0200

    Add test code

---
+++ README.md
@@ -0,0 +1,11 @@
+This repository contains (alongside this README) a single script file that has executable permissions.
+It should serve as example for an issue in go-git when used on Windows where the permissions of that script are reset to 0644 (from 0755) and the file is therefore marked as modified when running `git status`.
+
+## How to use
+
+```
+git clone git@github.com:zerok/mre-go-git-win-permissions.git
+cd mre-go-git-win-permissions
+go run .
+```
+
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/exec"
+
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
+)
+
+func main() {
+	os.RemoveAll("tmp")
+	_, err := git.PlainClone("tmp", false, &git.CloneOptions{
+		URL:           "https://github.com/zerok/mre-go-git-win-permissions.git",
+		ReferenceName: plumbing.NewBranchReferenceName("main"),
+		Depth:         50,
+	})
+	if err != nil {
+		log.Fatalf("Failed to clone repository into tmp folder: %s", err)
+	}
+
+	cmd := exec.Command("git", "status", "--short")
+	cmd.Dir = "tmp"
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Fatalf("Failed to run git status in tmp folder: %s", err)
+	}
+
+	if len(output) == 0 {
+		log.Printf("✅ Status after checkout UNCHANGED")
+		return
+	}
+	log.Fatalf("🚨 Status after checkout CHANGED: %s", string(output))
+}
